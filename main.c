33cede66009f5ded3020c82d55763df828c4f85d
KotaNch <ilacihancov01@gmail.com>
2026-06-08 14:22:01 +0500

    cyclic buffer

--- main.c
+++ main.c
@@ -1,134 +1,120 @@
-#include <linux/init.h>
+#include "linux/cleanup.h"
+#include "linux/gfp_types.h"
+#include "linux/types.h"
 #include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/mutex.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
-#include <linux/uaccess.h>
-#include <linux/device.h>
-
-#define MY_DEVICE_CLEAR _IO('k', 1)
-#define DEVICE_NAME "simpele_char_dev"
-#define BUFFER_SIZE 1024
-
-static dev_t  dev_num;
-static struct cdev my_cdev;
-static struct class *my_class = NULL; 
-static struct device *my_device = NULL;    
 
 
-static char device_buffer[BUFFER_SIZE];
-static int buffer_data_size = 0; 
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ilya");
+MODULE_DESCRIPTION("Cyclic Memory kernel Module");
+
+#define BUFFER_SIZE 4096
+
+static char *cyclic_buffer;
+static int write_ptr = 0;
+static int read_ptr = 0;
+static int available_bytes = 0;
+
+static DEFINE_MUTEX(buffer_mutex);
+
+static struct cdev cyclic_cdev;
+static dev_t dev_num;
 
 
-static int dev_open(struct inode *inode, struct file *file){ 
-   pr_info("%s: Device opened\n",DEVICE_NAME);
-   return 0;
-} 
 
-static int dev_release(struct inode *inode, struct file *file){
-   pr_info("%s: Device closed\n",DEVICE_NAME);
-   return 0;
+
+static ssize_t write_data(const char __user *user_buf, size_t count){
+   int i;
+   mutex_lock(&buffer_mutex);
+   for (i = 0; i <count; i++){
+      if (available_bytes >= BUFFER_SIZE){
+         pr_warn("Cyclic Buffer is full!\n");
+         break;
+      }
+      
+      get_user(cyclic_buffer[write_ptr], &user_buf[i]);
+      write_ptr = (write_ptr + 1) % BUFFER_SIZE;
+      available_bytes++;
+   }
+   mutex_unlock(&buffer_mutex);
+   return i;
 }
 
-static ssize_t dev_read(struct file *file, char __user *buffer, size_t len, loff_t *offset){
-   if (*offset >= buffer_data_size) return 0;
-   if (len > buffer_data_size - *offset) len = buffer_data_size - *offset;
-
-   if (copy_to_user(buffer,device_buffer + *offset,len) != 0) return -EFAULT;
-
-   *offset += len;
-   pr_info("%s: Read %zu bytes\n", DEVICE_NAME, len);
-    return len;   
+static ssize_t read_data(char __user *user_buf, size_t count){
+   int i;
+   mutex_lock(&buffer_mutex);
+   
+   for (i = 0; i < count; i++){
+      if (available_bytes <= 0){
+         break;
+      }
+      
+      put_user(cyclic_buffer[read_ptr], &user_buf[i]);
+      read_ptr = (read_ptr + 1) % BUFFER_SIZE;
+      available_bytes--;
+   }
+   
+   mutex_unlock(&buffer_mutex);
+   return i;
 }
 
 
-
-
-
-
-static ssize_t dev_write(struct file *file,const char __user *buffer, size_t len, loff_t *offset){
-   if (len > BUFFER_SIZE){
-      len = BUFFER_SIZE;
-   }
-   if (copy_from_user(device_buffer, buffer, len) != 0) {
-        return -EFAULT;
-   }
-   buffer_data_size = len;
-   *offset = len;
-
-
-   pr_info("%s: Written %zu bytes\n", DEVICE_NAME, len);
-   return len;
-}       
-
-
-static long dev_ioctl(struct file *file,unsigned int cmd, unsigned long arg){
-   switch (cmd) { 
-      case MY_DEVICE_CLEAR:
-         memset(device_buffer,0,BUFFER_SIZE);
-         buffer_data_size = 0;
-         pr_info("MY_DEVICE_CLEAR executed.\n");
-         return 0;
-      default:
-         return -EINVAL;
-   }            
-   return 0;
+static ssize_t cyclic_fops_write(struct file *f, const char __user *buf, size_t count, loff_t *offset){
+   return write_data(buf,count);
 }
- 
-static const struct file_operations fops = {
-   .owner  = THIS_MODULE,
-   .open    = dev_open,
-   .release = dev_release,
-   .read    = dev_read,    
-   .write = dev_write,
-   .unlocked_ioctl = dev_ioctl,
-}; 
 
+static ssize_t cyclic_fops_read(struct file *f, char __user *buf, size_t count, loff_t *offset){
+   return read_data(buf,count);
+}
 
-    
+static const struct file_operations cyclic_fops = {
+   .owner = THIS_MODULE,
+   .read = cyclic_fops_read,
+   .write = cyclic_fops_write,
+};
 
-static  int __init char_dev_init(void){
-   if (alloc_chrdev_region(&dev_num,0,1,DEVICE_NAME)<0){
-      return -1;
+static int __init cyclic_init(void){
+   int ret;
+   
+   cyclic_buffer = kmalloc(BUFFER_SIZE,GFP_KERNEL);
+   if (!cyclic_buffer){
+      return -ENOMEM;  
    }
-
-   cdev_init(&my_cdev,&fops);
-   if (cdev_add(&my_cdev,dev_num,1)<0){
-      unregister_chrdev_region(dev_num, 1);
-        return -1;
+   
+   ret = alloc_chrdev_region(&dev_num, 0,1,"cyclic_buf");
+   if (ret < 0 ){
+      kfree(cyclic_buffer);
+      return ret;
    }
-     
-   my_class = class_create(DEVICE_NAME);
-   if (IS_ERR(my_class)){
-      cdev_del(&my_cdev);
+   cdev_init(&cyclic_cdev,&cyclic_fops);
+   ret = cdev_add(&cyclic_cdev,dev_num,1);
+   if (ret < 0){
       unregister_chrdev_region(dev_num,1);
-      return PTR_ERR(my_class);
+      kfree(cyclic_buffer);
+      return ret;
    }
-
-   my_device = device_create(my_class, NULL, dev_num, NULL, DEVICE_NAME);
-    if (IS_ERR(my_device)) {
-        class_destroy(my_class);
-        cdev_del(&my_cdev);
-        unregister_chrdev_region(dev_num, 1);
-        return PTR_ERR(my_device);
-    }
-
-    pr_info("%s: Driver registered successfully\n", DEVICE_NAME);
-    return 0;
+   
+   pr_info("Module loaded successfully.\n");
+   return 0; 
 }
 
-static void __exit char_dev_exit(void) {
-    device_destroy(my_class, dev_num);
-    class_destroy(my_class);
-    cdev_del(&my_cdev);
-    unregister_chrdev_region(dev_num, 1);
-    pr_info("%s: Driver unregistered\n", DEVICE_NAME);
-}  
- 
- MODULE_LICENSE("GPL");
- MODULE_AUTHOR("Ilya");
- MODULE_DESCRIPTION("Hello world module");
- MODULE_VERSION("1.O");
 
 
-module_init(char_dev_init);
-module_exit(char_dev_exit); 
+static void __exit cyclic_exit(void){
+   cdev_del(&cyclic_cdev);                
+   unregister_chrdev_region(dev_num, 1); 
+   kfree(cyclic_buffer);
+   pr_info("Module unloaded successfully.\n");
+}
+
+
+module_init(cyclic_init);
+module_exit(cyclic_exit);
