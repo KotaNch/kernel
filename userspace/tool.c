4c2462260d14e9ba1dc15b0f4c62a2dc06d5e504
KotaNch <ilacihancov01@gmail.com>
2026-06-22 10:13:53 +0500

    feat(tool): add avail command via CYCLIC_IOC_AVAILABLE ioctl

--- userspace/tool.c
+++ userspace/tool.c
@@ -1,15 +1,16 @@
-#include "linux/types.h"
-#include <stdio.h>
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <unistd.h> 
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 
 #define DEVICE_PATH "/dev/cyclic_buffer"
-#define MY_DEVICE_CLEAR _IO('k', 1)  
+
+#define CYCLIC_IOC_MAGIC 'k'
+#define CYCLIC_IOC_CLEAR _IO(CYCLIC_IOC_MAGIC,1)
+#define CYCLIC_IOC_AVAILABLE  _IOR(CYCLIC_IOC_MAGIC, 2, int)
 
 static void usage(const char *prog){
     fprintf(stderr,
@@ -17,7 +18,8 @@ static void usage(const char *prog){
         " %s clear\n"
         " %s write <text\n"
         " %s read <bytes>\n",
-        prog,prog,prog
+        " %s avail\n",
+        prog,prog,prog,prog
     );
 }
 
@@ -31,11 +33,22 @@ static int open_device(void){
 }
 
 static int clear_buffer(int fd){
-    if (ioctl(fd, MY_DEVICE_CLEAR) <0){
+    if (ioctl(fd, CYCLIC_IOC_CLEAR) <0){
         fprintf(stderr,"ioctl clear: %s\n", strerror(errno));
         return 1;
     }
     puts("Buffer cleared");
+    return 0;
+}
+
+static int available_buffer(int fd){
+    int avail = 0;
+
+    if (ioctl(fd, CYCLIC_IOC_AVAILABLE, &avail)<0){
+        fprintf(stderr,"ioctl avail: %s\n", strerror(errno));
+        return 1;
+    }
+    printf("avaliable: %d bytes\n",avail);
     return 0;
 }
 
@@ -93,18 +106,21 @@ int main(int argc,char **argv) {
     if (fd < 0){
         return 1;
     }
+
     if (strcmp(argv[1],"clear") == 0 && argc == 2){
         ret = clear_buffer(fd);
     }else if (strcmp(argv[1], "write") == 0 && argc == 3){
         ret = write_buffer(fd, argv[2]);
     }else if (strcmp(argv[1], "read") == 0 && argc == 3) {
         ret = read_buffer(fd, argv[2]);
+    }else if (strcmp(argv[1], "avail") == 0 && argc == 2) {
+        ret = available_buffer(fd);
     }else {
-    usage(argv[0]);
-    ret = 1;
+        usage(argv[0]);
+        ret = 1;
     }
     close(fd);
     return ret;
 }
-
+ 
                                               
